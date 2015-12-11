df170fc867a0eba785d452f3deec27f94b92f3c1
Daman <daman.bareiss@utah.edu>
2015-12-11 10:23:22 -0700

    fixed minkowski corner case of overlapping normals, still need intersection tests

--- src/MinkowskiSum2d.cc
+++ src/MinkowskiSum2d.cc
@@ -13,7 +13,8 @@ MinkowskiSum2d::MinkowskiSum2d(const std::vector<Eigen::Vector2f>& points_in,
 
 std::vector<Eigen::Vector2f> MinkowskiSum2d::CalculateMinkowskiSum(void) {
 	if (original_points_.size() > 0) {
-		/*
+		RemoveOutliers();
+
 		std::vector<Eigen::Vector3f> normals(original_points_.size() - 1);
 		for (int i = 1; i < original_points_.size(); ++i) {
 			Eigen::Vector3f normal;
@@ -55,11 +56,7 @@ std::vector<Eigen::Vector2f> MinkowskiSum2d::CalculateMinkowskiSum(void) {
 			}
 		}
 		minkowski_points_.push_back(minkowski_points_.front());
-		*/
-		RemoveOutliers();
-		for (int i = 0; i < original_points_.size(); ++i) {
-			minkowski_points_.push_back(original_points_[i]);
-		}
+
 	}
 	return minkowski_points_;
 }  // CalculateMinkowskiSum
@@ -87,10 +84,10 @@ Eigen::Vector2f MinkowskiSum2d::FindIntersection(const Eigen::Vector2f& A,
 std::vector<Eigen::Vector2f> MinkowskiSum2d::FindCircle(const Eigen::Vector2f& A,
 																												const Eigen::Vector2f& B,
 																												const Eigen::Vector2f& C) {
-	Eigen::Vector2f normal_AB; normal_AB << -((A-B)/(A-B).norm())[1],
-															 ((A-B)/(A-B).norm())[0];
-	Eigen::Vector2f normal_BC; normal_BC << -((B-C)/(B-C).norm())[1],
-															 ((B-C)/(B-C).norm())[0];
+	Eigen::Vector2f normal_AB; normal_AB << -(A-B)[1], (A-B)[0];
+	normal_AB.normalize();
+	Eigen::Vector2f normal_BC; normal_BC << -(B-C)[1], (B-C)[0];
+	normal_BC.normalize();
 	
 	Eigen::Vector2f a2 = radius_*normal_AB;
 	Eigen::Vector2f b2 = radius_*normal_BC;
@@ -127,12 +124,66 @@ void MinkowskiSum2d::RemoveOutliers(void) {
 	// Jumps to the other side away from the point at the end of the narrow
 	// channel. When the channel is narrower than the value of 2r then
 	// the point at the end should be an outlier.
-	std::vector<Eigen::Vector2f>::iterator it = original_points_.begin();
-  for (int i = original_points_.size() - 1; i < original_points_.size(); ++i) {
-		std::cout << original_points_[i].transpose() << std::endl;
+	original_points_.pop_back();
+	std::vector<int> indices(original_points_.size());
+	for (int i = 0; i < indices.size(); ++i) {
+		indices[i] = i;
 	}
+	
+	std::vector<Eigen::Vector2f>::iterator it = original_points_.begin();
+	std::vector<int>::iterator it_two = indices.begin();
+	Eigen::Vector3f A,B,C;
+	Eigen::Vector3f n_ab, n_bc;
+  for (int i = 0; i < original_points_.size(); ++i) {
+		// If first point, compare to end
+		if (i == 0) {
+			A << original_points_.back()[0], original_points_.back()[1], 0;
+			//std::cout << "A: " << indices.back() << ", ";
+		} else {
+			A << original_points_[i - 1][0], original_points_[i - 1][1], 0;
+			//std::cout << "A: " << indices[i - 1] << ", ";
+		}
+		B << original_points_[i][0], original_points_[i][1], 0;
+		//std::cout << "B: " << indices[i] << ", ";
+		if (i == original_points_.size() - 1) {
+			C << original_points_.front()[0], original_points_.front()[1], 0;
+			//std::cout << "C: " << indices.front() << std::endl;
+		} else {
+			C << original_points_[i + 1][0], original_points_[i + 1][1], 0;
+			//std::cout << "C: " << indices[i + 1] << std::endl;
+		}
+		n_ab << -(A-B)[1], (A-B)[0], 0;
+		n_ab.normalize();
+		n_bc << -(B-C)[1], (B-C)[0], 0;
+		n_bc.normalize();
+		//std::cout << std::endl << "i: " << i << std::endl;
+		if ( (n_ab.cross(n_bc))[2] < 0.0 &&
+				 ((A + radius_*n_ab - B).cross(C-B))[2] < 0.0 ) {
+			/*
+			std::cout << "REMOVE" << std::endl;
+			std::cout << "n_ab: " << n_ab.transpose() << std::endl;
+			std::cout << "A: " << A.head(2).transpose() << std::endl;
+			std::cout << "B: " << B.head(2).transpose() << std::endl;
+			std::cout << "C: " << C.head(2).transpose() << std::endl;
+			std::cout << "r: " << radius_ << std::endl;
+			std::cout << "A + rn: " << (A + radius_*n_ab).head(2).transpose() << std::endl;
+			*/
+			original_points_.erase(it + i);
+			indices.erase(it_two + i);
+			i = 0;
+		}
+		/*
+		for (int j = 0; j < original_points_.size(); ++j) {
+			std::cout << indices[j] << ", ";
+		}
+		std::cout << std::endl;
+		*/
+	}
+	/*
 	int k;
 	std::cin >> k;
+	*/
+	original_points_.push_back(original_points_.front());
 }  // RemoveOutliers
 
 
